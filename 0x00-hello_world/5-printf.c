1d32d74c0d77b05787591d98730c88e47846dbe9
Slimanyy <aabdulqudduss@gmail.com>
2022-10-13 05:26:10 -0700

    using printfunction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  * prntf string
- * Return int after program has been executed
+ * Return: return 0 after program has been executed
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf ("with proper grammar, but the outcome is a piece of art,\n");
 return(0);
 }
