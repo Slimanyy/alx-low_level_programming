69af4cad699acc59d3b99f4dfc255272e8b37e4d
Slimanyy <aabdulqudduss@gmail.com>
2022-10-13 05:03:33 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * *main - entry point
+ * *
+ * *Return: 0 after printing the function
+ * */
 int main(void)
 {
-char st_g[] = "\"Programming is like building a multilingual puzzle\n";
-puts(st_g);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
