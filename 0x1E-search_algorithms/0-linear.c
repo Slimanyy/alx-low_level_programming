ca36190db44bda290a9120d995e4565f4a4f52c8
Slimanyy <aabdulqudduss@gmail.com>
2023-05-08 08:48:18 -0700

    tyvjd

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in a sorted array of integers
+ * Return: index of value or -1 if not found
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t inc;
+
+	if (array == NULL)
+		return (-1);
+	for (inc = 0; inc < size; inc++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", inc, array[inc]);
+		if (array[inc] == value)
+			return (inc);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+raedme fir c
